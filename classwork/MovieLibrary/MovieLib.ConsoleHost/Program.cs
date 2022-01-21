8fa2d2c539cfe8317cbec1a28af93f0c295ac881
michael.taylor <michael.taylor@my.tccd.edu>
2022-01-20 21:42:25 -0600

    Initial version

--- classwork/MovieLibrary/MovieLib.ConsoleHost/Program.cs
+++ classwork/MovieLibrary/MovieLib.ConsoleHost/Program.cs
@@ -1,13 +1,22 @@
-﻿using System;
+﻿/*
+ * Your Name
+ * ITSE 1430
+ * Lab 1
+ */
+using System;
 
 namespace MovieLib.ConsoleHost
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main ( string[] args )
         {
-            //A comment
+            //TODO: A comment
             Console.WriteLine("Hello World!");
+
+            //ANother block
+
+            //Yet another block
         }
     }
 }
