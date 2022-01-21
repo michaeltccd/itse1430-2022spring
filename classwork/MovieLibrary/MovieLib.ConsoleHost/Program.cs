1233db257d4787d95420099506ca5f7693644b7b
michael.taylor <michael.taylor@my.tccd.edu>
2022-01-20 20:22:07 -0600

    Initial version

---
+++ classwork/MovieLibrary/MovieLib.ConsoleHost/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MovieLib.ConsoleHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
